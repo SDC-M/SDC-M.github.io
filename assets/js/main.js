35742151af1c5fe42af4931421f82824f8e3fe6d
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-01-08 18:46:02 +0100

    Template

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+
+});
+
